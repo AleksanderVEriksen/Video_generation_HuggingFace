2cb85aa3d0c37c361f458199d1cf6469c133324e
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2025-06-30 02:13:20 +0200

    update req, exit and venv

---
+++ exit.ps1
@@ -0,0 +1,4 @@
+# Save the current environment's packages to requirements.txt
+pip freeze > requirements.txt
+# Exit the virtual environment
+deactivate
---
+++ venv.ps1
@@ -0,0 +1,7 @@
+# Append to venv and install dependencies
+
+# venv.ps1
+# Activate the virtual environment (for PowerShell)
+.\cuda\Scripts\Activate.ps1
+
+
